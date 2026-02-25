6fc4861f0e1c02a812466f7a2a7df3114a5e404d
soumilidas03 <soudasmili@gmail.com>
2026-02-25 19:22:42 +0530

    Rename src to agents and add utils file

---
+++ utils/groq_client.py
@@ -0,0 +1,11 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_groq_client():
+    api_key = os.getenv("GROQ_API_KEY")
+    if not api_key:
+        raise ValueError("GROQ_API_KEY not found")
+    return Groq(api_key=api_key)
